e4979eae51b7e21ed3bbc5ff6b9e0bf8bf3daa3f
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 15:05:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
-def reverse_each_word(string)
-  array2 = []
-  reverse_string = ""
-  array = string.split 
-  array.each do |word|
-    reverse_word = word.reverse 
-    array2 << reverse_word 
-    reverse_string = array2.join(" ")
-  end 
-  return reverse_string  
-end 
+# def reverse_each_word(string)
+#   array2 = []
+#   reverse_string = ""
+#   array = string.split 
+#   array.each do |word|
+#     reverse_word = word.reverse 
+#     array2 << reverse_word 
+#     reverse_string = array2.join(" ")
+#   end 
+#   return reverse_string  
+# end 
 
 def reverse_each_word(string)
   array = string.split 
